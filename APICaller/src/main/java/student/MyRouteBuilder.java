ee823b43fc919e105462f461def7e005e75b8739
Nikita Nesterov <nik.nest1@gmail.com>
2021-09-03 21:29:42 +0200

    added: json messages support

--- APICaller/src/main/java/student/MyRouteBuilder.java
+++ APICaller/src/main/java/student/MyRouteBuilder.java
@@ -12,23 +12,36 @@ public class MyRouteBuilder extends RouteBuilder {
      * Let's configure the Camel routing rules using Java code...
      */
     public void configure() {
-        Predicate personLondon = xpath("/person/city = 'London'");
-        Predicate personKarlsruhe = xpath("/person/city = 'Karlsruhe'");
+        Predicate personLondonxpath = xpath("/person/city = 'London'");
+        Predicate personLondonjsonpath = jsonpath("$..person[?(@.city=='London')]");
+//        Predicate personLondon = PredicateBuilder.or(personLondonxpath, personLondonjsonpath);
+
+        Predicate personKarlsruhexpath = xpath("/person/city = 'Karlsruhe'");
+        Predicate personKarlsruhejsonpath = jsonpath("$..person[?(@.city=='Karlsruhe')]");
+//        Predicate personKarlsruhe = PredicateBuilder.or(personKarlsruhexpath, personKarlsruhejsonpath);
+
+
         // here is a sample which processes the input files
         // (leaving them in place - see the 'noop' flag)
         // then performs content based routing on the message using XPath
         from("file:src/data?noop=true")
-            .choice()
-                .when(personLondon)
-                    .log("UK message")
-                    .transform().simple("foo")
-                    .to("file:target/messages/uk?fileName=uk.txt")
-                .when(personKarlsruhe)
-                    .log("DE message")
-                    .to("file:target/messages/de")
-                .otherwise()
-                    .log("Other message")
-                    .to("file:target/messages/others");
+                .choice()
+                    .when(personKarlsruhejsonpath)
+                        .log("UK message")
+                        .to("file:target/messages/uk")
+                    .when(personLondonxpath)
+                        .log("UK message")
+                        .transform().simple("foo")
+                        .to("file:target/messages/uk?fileName=uk.txt")
+                    .when(personKarlsruhejsonpath)
+                        .log("DE message")
+                        .to("file:target/messages/de")
+                    .when(personKarlsruhexpath)
+                        .log("DE message")
+                        .to("file:target/messages/de")
+                    .otherwise()
+                        .log("Other message")
+                        .to("file:target/messages/others");
     }
 
 }
